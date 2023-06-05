e8cfa3f74b5b6c4a4f7ae47f83d98347e3fab9d4
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2023-06-05 18:52:27 +0300

    Work Shop

---
+++ WorkShop/Demo.java
@@ -0,0 +1,21 @@
+package WorkShop;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+        list.addLast(5);
+
+        int[] ints = list.toArray();
+
+        System.out.println();
+
+    }
+}
+
